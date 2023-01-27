34e142ebb39f38350b946d238f7133515d473a99
Joseph <ikehajos@gmail.com>
2023-01-26 17:10:52 -0800

    Changed directory layout and added initial Employee API boilerplate

--- src/main/java/com/example/employees/EmployeeDemoApplication.java
+++ src/main/java/com/example/employees/EmployeeDemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.employees.Employee;
+package com.example.employees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/example/employees/controller/EmployeeController.java
@@ -0,0 +1,85 @@
+package com.example.employees.controller;
+
+import com.example.employees.exception.ResourceNotFoundException;
+import com.example.employees.model.Employee;
+import com.example.employees.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    /**
+     * Get all employees
+     * @return List of Employee objects
+     */
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    /**
+     * Create an employee object from the REST API
+     * @param employee The Employee object to be created
+     * @return The Employee object to be created
+     */
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    /**
+     * Find an employee by an ID
+     * @param id id to find employee by
+     * @return Employee found
+     */
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow();
+        return ResponseEntity.ok(employee);
+    }
+
+    /**
+     * Update information for an existing employee
+     * @param id ID of the employee to be updated
+     * @param employee new information about the employee
+     * @return the newly updated employee
+     */
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+        Employee employeeToUpdate = employeeRepository.findById(id).orElseThrow();
+
+        employeeToUpdate.setFirstName(employee.getFirstName());
+        employeeToUpdate.setLastName(employee.getLastName());
+        employeeToUpdate.setEmailId(employee.getEmailId());
+
+        Employee updatedEmployee = employeeRepository.save(employeeToUpdate);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    /**
+     * Deletes an employee with the corresponding ID
+     * @param id id to find employee to delete
+     * @return an OK response entity
+     */
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow();
+
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+}
